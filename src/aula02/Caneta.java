a4e17507a121407bbde05c5a0cd74d4fd22f061a
Arthur Vinicius <thuur.vss@gmail.com>
2021-03-26 19:49:13 -0300

    Aula 02 conceitos de POO com Java

---
+++ src/aula02/Caneta.java
@@ -0,0 +1,36 @@
+package aula02;
+
+public class Caneta {
+    String cor;
+    String modelo;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tamapda? " + this.tampada);
+    }
+
+    void rabiscar(){
+        if (this.tampada){
+            System.out.println("Caneta " + this.modelo + " tampada, não pode rabiscar...");
+        } else {
+            System.out.println("Rabiscando...");
+        }
+
+    }
+
+    void tampar(){
+        this.tampada = true;
+        System.out.println("Caneta " + this.modelo + " foi tampada.");
+    }
+
+    void destampar(){
+        this.tampada = false;
+        System.out.println("Caneta " + this.modelo + " foi destampada.");
+    }
+}
---
+++ src/aula02/aula02.java
@@ -0,0 +1,32 @@
+package aula02;
+
+public class aula02 {
+    public static void main(String[] args) {
+        // Instanciar objeto a partir da classe
+        Caneta c1 = new Caneta();
+
+        // Modificar os atributos
+        c1.modelo = "BIC";
+        c1.cor = "Azul";
+        c1.ponta = 05.f;
+        c1.tampada = true;
+
+        // Chama o métodos que mostra status do objeto
+        c1.status();
+        // Chama método rabiscar
+        c1.rabiscar();
+
+
+        // Novo objeto
+        Caneta c2 = new Caneta();
+        c2.modelo = "Hostnet";
+        c2.cor = "Preta";
+        c2.tampada = true;
+
+        c2.status();
+        c2.destampar();
+        c2.rabiscar();
+
+    }
+
+}
